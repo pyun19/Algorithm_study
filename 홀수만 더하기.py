38a740895c14bac5ce397b8f41bd11c7be1aed3e
pyun19 <62095363+pyun19@users.noreply.github.com>
2021-01-08 03:53:14 +0900

    2021_01_07
    홀수만 더하기

---
+++ 홀수만 더하기.py
@@ -0,0 +1,10 @@
+import sys
+sum = 0
+T = int(input())
+for i in range(1, T + 1):
+    a = input().split(' ')
+    for j in range(len(a)):
+        if int(a[j]) %2 == 1:
+            sum += int(a[j])
+    print('#%d'%i, sum)
+    sum = 0
