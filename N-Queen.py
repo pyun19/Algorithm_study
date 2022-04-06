0f43683469ae20d26ae28f5076b2a07a58277aef
pyun19 <62095363+pyun19@users.noreply.github.com>
2022-04-06 23:38:15 +0900

    2022_04_06

---
+++ N-Queen.py
@@ -0,0 +1,36 @@
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+# 0 0 0 0 0 0 0 0
+
+
+
+# import sys
+#
+# n = int(sys.stdin.readline())
+# queen = [[0 for x in range(n)] for x in range(n)]
+# print(*queen, sep='\n')
+#
+# for i in range(n):
+#     queen[i]
+#     if queen[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+b = 2
